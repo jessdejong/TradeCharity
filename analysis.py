93e4c6b4c95f4d5c9a390b48604abed2788c6563
Jess DeJong <jessdejong843@gmail.com>
2019-02-16 17:16:48 -0600

    modified:   analysis.py

--- analysis.py
+++ analysis.py
@@ -3,42 +3,19 @@ from watson_developer_cloud import NaturalLanguageUnderstandingV1
 from watson_developer_cloud.natural_language_understanding_v1 import Features, EntitiesOptions, KeywordsOptions
 import pprint
 
-'''
-natural_language_processing = NaturalLanguageUnderstandingV1(
-        username='a058b551-a067-48ad-9c08-3d57cb331a12',
-        password='nW4Q8XpczFc4',
-        version='2017-02-27')
-'''
-
+# setup API
 natural_language_processing = NaturalLanguageUnderstandingV1(
     version='2018-11-16',
     iam_apikey='VF3FzeOIqt7yesb3WDtvYRP283LjPFWBC37lOS19jbi8',
     url='https://gateway.watsonplatform.net/natural-language-understanding/api')
 
-'''def getNewsTitles():
-    tweets = twitter_wrapper.getUserTweets(username)
-    urls = twitter_wrapper.getUserLinks(username)
-'''
-
-def analyze(titleText):
+# analyze text
+def analyze(textString):
     response = natural_language_processing.analyze(
-        text = titleText,
+        text = textString,
         features=Features(
             entities=EntitiesOptions(emotion=True, sentiment=True, limit=2),
             keywords=KeywordsOptions(emotion=True, sentiment=True, limit=2))).get_result()
-    '''
-        response = natural_language_understanding.analyze(
-
-                text = titleText,
-
-                features = Features(
-                    keywords=KeywordsOptions(
-                        emotion=True,
-                        sentiment=True,
-                        limit=1)),
-                    
-                language = 'en')
-    '''
     data = json.loads(json.dumps(response, indent=2))
     return data
 
