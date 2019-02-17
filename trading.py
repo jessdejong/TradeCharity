f05ce61654c56f36d9f8260fe5985000fd91adf5
Jess DeJong <jessdejong843@gmail.com>
2019-02-17 01:19:53 -0600

    trading simulation

--- trading.py
+++ trading.py
@@ -5,8 +5,6 @@ import analysis
 
 def backtest(start_year, start_month, start_day, money):
     """Performs a backtest from a certain date, returns article info and start and end price"""
-    start = dt.datetime(start_year, start_month, start_day)
-    end = dt.datetime.now()
 
     articles = nw.get_article_info(start_year, start_month, start_day)
 
@@ -30,25 +28,47 @@ def backtest(start_year, start_month, start_day, money):
         articleDescription = articles[i][3]
         articleURL = articles[i][4]
         articleDate = articles[i][5]
+        date = articleDate.split("-")
+        #print(date)
 
         # sentiment analysis
         sentiment = analysis.analyze(articleDescription)
-        print(articleDescription)
-        print(sentiment)
-
         
-    
-    
+        if (sentiment != 0):
+            start = dt.datetime(int(date[0]), int(date[1]), int(date[2][0:date[2].find("T")]))
+            end = dt.datetime(int(date[0]), int(date[1]), int(date[2][0:date[2].find("T")]))
+            timeSeries = web.DataReader(sticker, 'iex', start, end)
+            #print(timeSeries['open'].values[0])
+
+            openVal = timeSeries['open'].values[0]
+            closeVal = timeSeries['close'].values[0]
+
+            if (sentiment > 0):
+                """ buy the stock """
+                if openVal <= money:
+                    print("buy")
+                    print(closeVal - openVal)
+                    print(openVal)
+                    print(closeVal)
+                    money += (closeVal - openVal) 
+            elif (sentiment < 0):
+                """ short the stock """
+                if openVal <= money:
+                    print("short")
+                    print(openVal - closeVal)
+                    print(openVal)
+                    print(closeVal)
+
+                    money += (openVal - closeVal)
+
+            print(articleDescription)
+            print(money)
 
     #aapl = web.DataReader('AAPL', 'iex', start, end)
 
 #def getNextTrade():
-    
 
-
-backtest(2019, 1, 1, 1000)
-
-
+backtest(2019, 0, 0, 1000)
 
 #print(aapl.shape)
 #print(aapl.head())
